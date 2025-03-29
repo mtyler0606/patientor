a195f7a4d6a1439ebafbda8d10203f870b722cf8
mtyler0606 <mtyler0606@gmail.com>
2025-03-28 19:21:16 -0500

    9.25: Patientor, step 5

--- src/components/SinglePatient.tsx
+++ src/components/SinglePatient.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
-import { Patient, Entry } from "../types";
+import { Patient, Entry, Diagnosis } from "../types";
 import axios from "axios";
 import { apiBaseUrl } from "../constants";
 import patientService from "../services/patients";
+import diagnosesService from "../services/diagnoses";
 import { useParams } from "react-router-dom";
+
 
 interface EntryProps {
     entry: Entry;
+    diagnoses: Diagnosis[] | undefined;
 }
+const getDiagnoses = (code:string, diagonses: Diagnosis[] ) => {
+    const diagnosis = diagonses.find(diagnosis => diagnosis.code === code);
+    if(diagnosis){
+        return diagnosis.name;
+    }
+};
+const toDiagnonsesArray = (arr: Diagnosis[] | undefined) => {
+    if(arr){
+        return arr;
+    }
+    else {
+        const arr = [] as Diagnosis[];
+        return arr;
+    }
+};
 
 const ShowEntry = (props: EntryProps) => {
     return (
@@ -15,7 +33,7 @@ const ShowEntry = (props: EntryProps) => {
         <p>{props.entry.date} {props.entry.description}</p>
         {props.entry.diagnosisCodes &&
             <ul>
-            {props.entry.diagnosisCodes.map((entry, index) => <li key={index}>{entry}</li>)}
+            {props.entry.diagnosisCodes.map((entry, index) => <li key={index}>{entry} {getDiagnoses(entry, toDiagnonsesArray(props.diagnoses))}</li>)}
         </ul>
 }
         </>
@@ -24,6 +42,7 @@ const ShowEntry = (props: EntryProps) => {
 const SinglePatient = () => {
 
     const [patient, setPatient] = useState<Patient>();
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
     const id = useParams().id as string;
 
@@ -33,9 +52,12 @@ const SinglePatient = () => {
     const fetchPatient = async () => {
       const patient = await patientService.getPatient(id);
       setPatient(patient);
+      const allDiagnoses = await diagnosesService.getAllDiagnoses();
+      setDiagnoses(allDiagnoses);
     };
     void fetchPatient();
   }, []);
+
   if(patient){
     return (
         <>
@@ -43,7 +65,7 @@ const SinglePatient = () => {
         <p>ssn : {patient.ssn}</p>
         <p>occupation: {patient.occupation}</p>
         <h3>entries</h3>
-        {patient.entries.map(entry => <ShowEntry entry={entry} />)}
+        {patient.entries.map(entry => <ShowEntry entry={entry} diagnoses={diagnoses} />)}
         </>
     );
 }
